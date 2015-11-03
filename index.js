e3e37e4fd9a4d103e90aada96b2571c7a0bb5d5c
Mingard <webservices@arthurmingard.com>
2015-11-03 18:17:42 +0000

    sample added, breakout formatting into module, add type switch

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 module.exports = require('./lib/layar');
+var sample = require('./lib/sample.js');
 
 var example = function() {
-	var layarExample = module.exports.layar({
-		string: "This is a string with some bold text. There are two paragraphs here.",
-		format: [
-			{
-				type: "b",
-			    	start: 27,
-			    	end: 37,
-			    	level: 1
-			},
-			{
-				type: "p",
-			    	start: 37,
-			    	end: 68,
-			    	level: 0
-			},
-			{
-				type: "p",
-			    	start: 0,
-			    	end: 37,
-			    	level: 0
-			}
-		]
-	});
+	var layarExample = module.exports.layar(sample.data);
 	layarExample.run();
 }
 
